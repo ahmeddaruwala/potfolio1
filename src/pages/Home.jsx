4ba05487f004ca3f17a3f35dbccb8a14bc589b58
ahmeddaruwala <ahmeddaruwala360@gmail.com>
2025-06-13 14:10:19 +0500

    portfolio

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import { Background } from "../components/Background"
+import { Navbar } from "../components/Navbar"
+import { Theme } from "../components/Theme"
+
+export const Home = () => {
+    return <div>
+        {/* Theme Toggle */}
+        <Theme />
+
+        {/* Background Effects */}
+        <Background />
+
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Main Content */}
+
+        {/* Footer */}
+    </div>
+}
