656cc3ea9da9cc841043fdeb6bb164234b68380f
ahmeddaruwala <ahmeddaruwala360@gmail.com>
2025-06-22 07:54:39 +0500

    portfolio

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,9 @@
+import { ExternalLink, Github } from "lucide-react";
+
 const projects = [
   {
     id: 1,
-    title: "e-commerce website",
+    title: "E-commerce Website",
     description: "under construction e-commerce website.",
     image: "/projects-images/project1.png",
     demoURL: "https://extra-small-hand.surge.sh/",
@@ -9,18 +11,18 @@ const projects = [
   },
   {
     id: 2,
-    title: "#",
-    description: "#",
-    image: "#",
-    demoURL: "#",
+    title: "",
+    description: "",
+    image: "",
+    demoURL: "",
     githubURL: "https://github.com/ahmeddaruwala",
   },
   {
     id: 3,
-    title: "#",
-    description: "#",
-    image: "#",
-    demoURL: "#",
+    title: "",
+    description: "",
+    image: "",
+    demoURL: "",
     githubURL: "https://github.com/ahmeddaruwala",
   },
 ];
@@ -39,7 +41,45 @@ export const Projects = () => {
         </p>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            
+          {projects.map((project, key) => (
+            <div
+              key={key}
+              className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
+            >
+              <div className="h-48 overflow-hidden">
+                <img
+                  src={project.image}
+                  alt={project.title}
+                  className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                />
+              </div>
+
+              <div className="p-6">
+                <h3 className="text-xl font-semibold mb-1">{project.title}</h3>
+                <p className="text-muted-foreground text-sm mb-4">
+                  {project.description}
+                </p>
+                <div className="flex justify-between items-center">
+                  <div className="flex space-x-3">
+                    <a
+                      href={project.demoURL}
+                      target="_blank"
+                      className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                    >
+                      <ExternalLink size={20} />
+                    </a>
+                    <a
+                      href={project.githubURL}
+                      target="_blank"
+                      className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                    >
+                      <Github size={20} />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
